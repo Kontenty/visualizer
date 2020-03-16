2f72feaef7873ec2079ee76ec245a200ce132b7b
Konrad Gorski <konradgrsk@gmail.com>
2020-03-16 19:56:57 +0100

    markers centering

--- src/components/map/Markers.jsx
+++ src/components/map/Markers.jsx
@@ -6,6 +6,8 @@ import { roundTo } from 'helpers'
 
 const StyledMarker = styled(Marker)`
   font-size: ${props => roundTo((props.fontBase + 2) * 2)}px;
+  text-align: center;
+  line-height: 1;
 `
 
 const Markers = ({ data, zoom }) => {
@@ -16,6 +18,8 @@ const Markers = ({ data, zoom }) => {
           key={`marker-${index}`}
           longitude={netPos.coords[0]}
           latitude={netPos.coords[1]}
+          offsetLeft={-7}
+          offsetTop={-7}
           fontBase={zoom}
         >
           {netPos.name}
